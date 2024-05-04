0a8820b3966095782ac87833eaaa68619d104f7d
Dmitry Klimovsky <dmitryklimovsky05@gmail.com>
2024-05-04 17:31:48 +0300

    added precision metric

--- MonteCarloMethod/src/main.cpp
+++ MonteCarloMethod/src/main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <mutex>
 #include <random>
+#include <math.h>
 
 std::mutex mtx;
 long long num_threads;
@@ -14,11 +15,11 @@ long long in_circle = 0;
 void Simulate(long long thread_id, long long points_per_thread)
 {
     std::default_random_engine generator;
-    std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    std::uniform_real_distribution<float> distribution(0.0, 1.0);
     for (long long i = 0; i < points_per_thread; i++)
     {
-        double x = distribution(generator);
-        double y = distribution(generator);
+        float x = distribution(generator);
+        float y = distribution(generator);
         if (x * x + y * y <= 1.0)
         {
             mtx.lock();
@@ -28,16 +29,22 @@ void Simulate(long long thread_id, long long points_per_thread)
     }
 }
 
+inline double eval_prob(double epsilon) 
+{
+    return std::min((M_PI / 4) * (1 - (M_PI / 4)) / (num_points * epsilon * epsilon), 1.0);
+}
+
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
+    if (argc != 4)
     {
-        std::cout << "Usage: " << argv[0] << " <number_of_threads> <number_of_points>" << std::endl;
+        std::cout << "Usage: " << argv[0] << " <number_of_threads> <number_of_points> <epsilon>" << std::endl;
         return 1;
     }
 
     num_threads = std::stoll(argv[1]);
     num_points = std::stoll(argv[2]);
+    double epsilon = std::stod(argv[3]);
 
     std::vector<std::thread> threads(num_threads);
     long long points_per_thread = num_points / num_threads;
@@ -60,4 +67,5 @@ int main(int argc, char *argv[])
     double pi = 4.0 * in_circle / num_points;
     std::cout << "Pi: " << pi << std::endl;
     std::cout << "Time: " << duration << " ms" << std::endl;
+    std::cout << "Precision : " << eval_prob(epsilon) << std::endl;
 }
